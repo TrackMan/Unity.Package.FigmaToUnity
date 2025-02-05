9fafcc6ac88719890f08277451b921c6e57cbf59
Anton Dyachenko <ada@trackman.com>
2025-02-05 13:16:13 +0100

    Added virtual keyword to the Sync methods in abstract classes

--- Runtime/Interface/Core/SyncElements.cs
+++ Runtime/Interface/Core/SyncElements.cs
@@ -7,7 +7,7 @@ namespace Figma
     public abstract class SyncVisualElement<T> : SubVisualElement, ISyncElement<T>
     {
         #region Methods
-        public void Sync(VisualElement parent, IEnumerable<T> data) => VisualElementExtensions.Sync(this, parent, data);
+        public virtual void Sync(VisualElement parent, IEnumerable<T> data) => VisualElementExtensions.Sync(this, parent, data);
         public abstract bool IsVisible(int index, T data);
         #endregion
     }
@@ -15,7 +15,7 @@ namespace Figma
     public abstract class SyncVisualElement<TCreationData, TData> : SubVisualElement, ISyncElement<TCreationData, TData>
     {
         #region Methods
-        public void Sync(VisualElement parent, TCreationData creationData, IEnumerable<TData> data) => VisualElementExtensions.Sync(this, parent, creationData, data);
+        public virtual void Sync(VisualElement parent, TCreationData creationData, IEnumerable<TData> data) => VisualElementExtensions.Sync(this, parent, creationData, data);
         public abstract void Initialize(int index, TCreationData creationData);
         public abstract bool IsVisible(int index, TData data);
         #endregion
@@ -24,7 +24,7 @@ namespace Figma
     public abstract class SyncButton<TData> : SubButton, ISyncElement<TData>
     {
         #region Methods
-        public void Sync(VisualElement parent, IEnumerable<TData> data) => VisualElementExtensions.Sync(this, parent, data);
+        public virtual void Sync(VisualElement parent, IEnumerable<TData> data) => VisualElementExtensions.Sync(this, parent, data);
         public abstract bool IsVisible(int index, TData data);
         #endregion
     }
@@ -32,7 +32,7 @@ namespace Figma
     public abstract class SyncButton<TCreationData, TData> : SubButton, ISyncElement<TCreationData, TData>
     {
         #region Methods
-        public void Sync(VisualElement parent, TCreationData creationData, IEnumerable<TData> data) => VisualElementExtensions.Sync(this, parent, creationData, data);
+        public virtual void Sync(VisualElement parent, TCreationData creationData, IEnumerable<TData> data) => VisualElementExtensions.Sync(this, parent, creationData, data);
         public abstract void Initialize(int index, TCreationData creationData);
         public abstract bool IsVisible(int index, TData data);
         #endregion
@@ -41,8 +41,8 @@ namespace Figma
     public abstract class SyncButtonSimple<TData> : SubButton, ISyncElement<Action<int>, TData>
     {
         #region Methods
-        public void Sync(VisualElement parent, Action<int> creationData, IEnumerable<TData> data) => VisualElementExtensions.Sync(this, parent, creationData, data);
-        public void Initialize(int index, Action<int> creationData) => clicked += () => creationData(index);
+        public virtual void Sync(VisualElement parent, Action<int> creationData, IEnumerable<TData> data) => VisualElementExtensions.Sync(this, parent, creationData, data);
+        public virtual void Initialize(int index, Action<int> creationData) => clicked += () => creationData(index);
         public abstract bool IsVisible(int index, TData data);
         #endregion
     }
@@ -50,7 +50,7 @@ namespace Figma
     public abstract class SyncLabel<TData> : SubLabel, ISyncElement<TData>
     {
         #region Methods
-        public void Sync(VisualElement parent, IEnumerable<TData> data) => VisualElementExtensions.Sync(this, parent, data);
+        public virtual void Sync(VisualElement parent, IEnumerable<TData> data) => VisualElementExtensions.Sync(this, parent, data);
         public abstract bool IsVisible(int index, TData data);
         #endregion
     }
@@ -58,7 +58,7 @@ namespace Figma
     public abstract class SyncLabel<TCreationData, TData> : SubLabel, ISyncElement<TCreationData, TData>
     {
         #region Methods
-        public void Sync(VisualElement parent, TCreationData creationData, IEnumerable<TData> data) => VisualElementExtensions.Sync(this, parent, creationData, data);
+        public virtual void Sync(VisualElement parent, TCreationData creationData, IEnumerable<TData> data) => VisualElementExtensions.Sync(this, parent, creationData, data);
         public abstract void Initialize(int index, TCreationData creationData);
         public abstract bool IsVisible(int index, TData data);
         #endregion
@@ -67,7 +67,7 @@ namespace Figma
     public abstract class SyncScrollView<TData> : SubScrollView, ISyncElement<TData>
     {
         #region Methods
-        public void Sync(VisualElement parent, IEnumerable<TData> data) => VisualElementExtensions.Sync(this, parent, data);
+        public virtual void Sync(VisualElement parent, IEnumerable<TData> data) => VisualElementExtensions.Sync(this, parent, data);
         public abstract bool IsVisible(int index, TData data);
         #endregion
     }
@@ -75,7 +75,7 @@ namespace Figma
     public abstract class SyncScrollView<TCreationData, TData> : SubScrollView, ISyncElement<TCreationData, TData>
     {
         #region Methods
-        public void Sync(VisualElement parent, TCreationData creationData, IEnumerable<TData> data) => VisualElementExtensions.Sync(this, parent, creationData, data);
+        public virtual void Sync(VisualElement parent, TCreationData creationData, IEnumerable<TData> data) => VisualElementExtensions.Sync(this, parent, creationData, data);
         public abstract void Initialize(int index, TCreationData creationData);
         public abstract bool IsVisible(int index, TData data);
         #endregion
